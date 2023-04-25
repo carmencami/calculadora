95b96ef658934a27db362227e5d75aa3d44dbca6
carmencami <105524744+carmencami@users.noreply.github.com>
2023-04-25 21:08:45 +0200

    min 4:28

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import FreeCodeCamp_logo from "../src/imagenes/FreeCodeCamp_logo.svg.png"
+import Boton from './componentes/Boton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+        src={FreeCodeCamp_logo}
+        className="freecodecamp-logo"
+        alt="logo-calculadora"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+      <div className="fila">
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+
+      </div>
+      <div className="fila"></div>
+      <div className="fila"></div>
+      <div className="fila"></div>
+      <div className="fila"></div>
+
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componentes/Boton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Boton.css"
+
+function Boton(props){
+    const esOperador=valor=>{
+        return isNaN(valor) & (valor != ".") && (valor != "=");
+    }
+    return(
+        <div 
+        className={`boton-contenedor ${esOperador(props.children)? "operador" : ""}`.trimEnd()}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Boton;
