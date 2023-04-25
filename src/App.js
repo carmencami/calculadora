7f699c3b763af77ce2194bc72c9d4f03366ea694
carmencami <105524744+carmencami@users.noreply.github.com>
2023-04-25 23:10:06 +0200

    faltran cosas aun

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import FreeCodeCamp_logo from "../src/imagenes/FreeCodeCamp_logo.svg.png"
-import Boton from './componentes/Boton';
+import Boton from './componentes/Boton.js';
+import Pantalla from './componentes/Pantalla.js';
+import BotonClear from './componentes/BotonClear.js';
+import { useState } from 'react';
 
 function App() {
+const [input, setInput]=useState('');
+
+const agregarInput = valor =>{
+  setInput(input + valor);
+};
+
+
   return (
     <div className='App'>
       <div className="freecodecamp-logo-contenedor">
@@ -13,17 +23,34 @@ function App() {
         />
       </div>
       <div className="contenedor-calculadora">
+        <Pantalla input={input}/>
       <div className="fila">
-        <Boton>1</Boton>
-        <Boton>+</Boton>
-
+        <Boton manejarClic={agregarInput}>1</Boton>
+        <Boton manejarClic={agregarInput}>2</Boton>
+        <Boton manejarClic={agregarInput}>3</Boton>
+        <Boton manejarClic={agregarInput}>+</Boton>
       </div>
-      <div className="fila"></div>
-      <div className="fila"></div>
-      <div className="fila"></div>
-      <div className="fila"></div>
-
-
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>4</Boton>
+        <Boton manejarClic={agregarInput}>5</Boton>
+        <Boton manejarClic={agregarInput}>6</Boton>
+        <Boton manejarClic={agregarInput}>-</Boton>
+      </div>
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>7</Boton>
+        <Boton manejarClic={agregarInput}>8</Boton>
+        <Boton manejarClic={agregarInput}>9</Boton>
+        <Boton manejarClic={agregarInput}>*</Boton>
+      </div>
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>=</Boton>
+        <Boton manejarClic={agregarInput}>0</Boton>
+        <Boton manejarClic={agregarInput}>.</Boton>
+        <Boton manejarClic={agregarInput}>/</Boton>
+      </div>
+      <div className="fila">
+        <BotonClear>Clear</BotonClear>
+      </div>
       </div>
     </div>
   );
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -8,6 +8,7 @@ function Boton(props){
     return(
         <div 
         className={`boton-contenedor ${esOperador(props.children)? "operador" : ""}`.trimEnd()}>
+            onClick={()=>props.manejarClic(props.children)}
             {props.children}
         </div>
     )
